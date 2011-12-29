a98aa85c6f37f3482786d48371e120debf1553e9
Daneel Olivaw <daneel.olivaw.r@gmail.com>
2011-12-29 12:00:00 +0100

    Cambiato il sistema di aggiornamento

--- aggiornaprogramma.rb
+++ aggiornaprogramma.rb
@@ -1,6 +1,14 @@
 def aggiornaprogramma(finestra, modo)
 	#puts "aggiornaprogramma"
-	if `git remote update`.upcase.include?("COUNTING OBJECTS")
+	output = `git fetch 2>&1`
+	if output.to_s != ""
+		puts output
+		Parameters.update("1", {:aggiornamento => "1"})
+	end
+	if Parameters.parametri.aggiornamento == true
+		puts "Aggiornamento pronto"
+		#puts "Output = #{output}"
+#	if output.include?("5")
 		#puts "Ci sono modifiche"
 		avviso = Gtk::MessageDialog.new(finestra, Gtk::Dialog::DESTROY_WITH_PARENT, Gtk::MessageDialog::QUESTION, Gtk::MessageDialog::BUTTONS_YES_NO, "Ci sono degli aggiornamenti al programma. Li scarico ora?")
 		risposta = avviso.run
@@ -8,6 +16,7 @@ def aggiornaprogramma(finestra, modo)
 		if risposta == Gtk::Dialog::RESPONSE_YES
 			`git pull`
 			Conferma.conferma(finestra, "Aggiornamento eseguito con successo. Chiudere il programma e riavviarlo.")
+			Parameters.update("1", {:aggiornamento => "0"})
 		else
 			Conferma.conferma(finestra, "L'aggiornamento non sarà eseguito.")
 		end
