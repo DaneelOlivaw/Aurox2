67118e637d87f50f8fb1b02f5ce48245475f9793
Daneel Olivaw <daneel.olivaw.r@gmail.com>
2011-12-29 11:17:51 +0100

    Modifiche di test 2

--- aggiornaprogramma.rb
+++ aggiornaprogramma.rb
@@ -1,6 +1,6 @@
 def aggiornaprogramma(finestra, modo)
 	#puts "aggiornaprogramma"
-	if `git remote update`.include?("Counting objects")
+	if `git remote update`.upcase.include?("COUNTING OBJECTS")
 		#puts "Ci sono modifiche"
 		avviso = Gtk::MessageDialog.new(finestra, Gtk::Dialog::DESTROY_WITH_PARENT, Gtk::MessageDialog::QUESTION, Gtk::MessageDialog::BUTTONS_YES_NO, "Ci sono degli aggiornamenti al programma. Li scarico ora?")
 		risposta = avviso.run
